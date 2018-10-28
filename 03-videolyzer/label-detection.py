a857985b2bf71b976adc368337d2c027049a8009
hollarado <hollar.steve@gmail.com>
2018-10-28 14:50:23 -0700

    Add example code for Rekgoniton

---
+++ 03-videolyzer/label-detection.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+bucket = s3.create_bucket(Bucket='hollaradovideozervideos')
+from pathlib import Path
+get_ipython().run_line_magic('ls', '/Users/IT/Downloads/*.mp4')
+get_ipython().run_line_magic('ls', '\\Users\\IT\\Downloads\\*.mp4')
+pathname = '~\Downloads\Blurry Video Of People Working.mp4'
+path = Path(pathname).expanduser().resolve()
+print(path)
+bucket.upload_file(str(path), str(path.name))
+rekognition_client = session.client('rekognition')
+response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket' : bucket.name, 'Name': path.name}})
+response
+job_id = response['JobId']
+result = rekognition_client.get_label_detection(JobId=job_id)
+result
+result.keys()
+result['JobStatus']
+result['ResponseMetadata']
